d017482e365520a9c4ad24cbb848f6eb7f934ac2
minim0no <ahmed.idrees3110@gmail.com>
2024-03-10 23:37:18 -0500

    Implement feature to upload and view processed images, minimize uuid

--- abstract-web-client/app/api/upload.ts
+++ abstract-web-client/app/api/upload.ts
@@ -1,16 +1,42 @@
 import { User } from "firebase/auth";
-import { randomUUID } from "crypto";
+import { v4 as uuidv4 } from "uuid";
 
-export async function uploadImage(user: User | null, image: File) {
-    const inputFileName = `${
-        user?.uid ? user.uid : ""
-    }-${Date.now()}-${randomUUID()}.${image.name.split(".").pop()}`; // UID -
+export async function processImage(user: User | null, image: File) {
+    const data = await uploadImage(user, image);
 
     try {
-        const data = {
-            inputFileName: inputFileName,
-        };
+        await fetch("http://127.0.0.1:5000/process-image", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
 
+        return data.inputFileName;
+    } catch (err) {
+        console.error("ERROR:", err);
+    }
+
+    return undefined;
+}
+
+/**
+ *
+ * @param user - The user object, if it exists
+ * @param image - The image the user is uploading
+ * @returns The json of the filename of the image in the cloud storage bucket
+ */
+async function uploadImage(user: User | null, image: File) {
+    const inputFileName = `${user?.uid ? user.uid : ""}-${Date.now()}-${
+        uuidv4().split("-")[0]
+    }.${image.name.split(".").pop()}`; // USER ID - TIME IN SECONDS - RANDOM.EXTENSION
+
+    const data = {
+        inputFileName: inputFileName,
+    };
+
+    try {
         // fetch signed url
         const response = await fetch("http://127.0.0.1:5000/get-upload-url", {
             method: "PUT",
@@ -33,6 +59,8 @@ export async function uploadImage(user: User | null, image: File) {
 
         alert("uploaded baby");
     } catch (err) {
-        console.log("error: ", err);
+        console.error("ERROR: ", err);
     }
+
+    return data;
 }
--- abstract-web-client/app/discover/upload/upload.tsx
+++ abstract-web-client/app/discover/upload/upload.tsx
@@ -1,15 +1,23 @@
 "use client";
 import { useAuth } from "@/app/components/context/AuthContext";
-import { uploadImage } from "@/app/api/upload";
+import { processImage } from "@/app/api/upload";
+import { useState } from "react";
 
 export default function Upload() {
     const user = useAuth();
+    const imageLinkPreset =
+        "https://storage.googleapis.com/abstract-processed-image-bucket/processed-";
+
+    const [imageLink, setImageLink] = useState<string | undefined>(undefined);
 
     const handleUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.item(0);
 
         if (file) {
-            uploadImage(user, file);
+            const filename = await processImage(user, file);
+            if (filename !== undefined) {
+                setImageLink(imageLinkPreset + filename);
+            }
         }
 
         event.target.files = null;
@@ -30,6 +38,7 @@ export default function Upload() {
                 className="hidden"
                 onChange={handleUpload}
             />
+            {imageLink && <img src={imageLink} alt="processed image" />}
         </>
     );
 }
