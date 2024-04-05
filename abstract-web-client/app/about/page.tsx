06d46fc85864b5c87ccc288ca2ef37215b827ec6
JaX-Y1 <136761731+JaX-Y1@users.noreply.github.com>
2024-04-04 19:20:26 -0500

    Added About Tab and removed test functon in processing.py

--- abstract-web-client/app/about/page.tsx
+++ abstract-web-client/app/about/page.tsx
@@ -1,3 +1,20 @@
 export default function About() {
-    return <div className="flex flex-col items-center">About</div>;
+    return (<div>
+        <div className="flex flex-col place-items-center">
+            <h1 className="h-12 text-4xl">About</h1>
+            <div>
+            <p className="mx-20 text-center text-balance font-sans">abstract is an image to art tool that lets you convert an image into <b>any</b> form of art!</p>
+            </div>
+            <div className="size-96 flex flex-col place-items-center justify-center">
+                <img className="border-4 border-slate-300 rounded-md"src="./halfLebronpixel.jpg"/>
+            </div>
+            <h1 className="h-12 text-4xl">Team</h1>
+            <div>
+            <p className="mx-20 text-center text-balance font-sans">This wonderful project was made possible by the Aggie Coding Club at Texas A&M University.</p>
+            </div>
+            <div className="size-96 flex flex-col place-items-center justify-center">
+                <img className="border-4 border-slate-300 rounded-md"src="./pixelTeam.jpg"/>
+            </div>
+            </div>
+        </div>);
 }
